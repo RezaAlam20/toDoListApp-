8d34218e1edcb1a0053b89ed3fe9f7a7f02fadd7
RezaAlam20 <rzaalam384@gmail.com>
2025-11-05 19:03:29 +0330

    some imports

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import styleSheet from "./style.css"
+import { projectHandler } from "./projectHandler"
+import { projects } from "./projectHandler"
+
+
+
+
+projectHandler()
+
+
+document.addEventListener("click" , ()=> console.log(projects))
+
+
