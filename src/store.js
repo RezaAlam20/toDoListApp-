1a4eb3969a0bd18ec404131f97865d90d5084953
RezaAlam20 <rzaalam384@gmail.com>
2025-11-05 23:00:13 +0330

    moved the form to a separate function line 14-23 / added a project remover function line 40-50

--- src/store.js
+++ src/store.js
@@ -11,29 +11,44 @@ let activeProject = projects[-1]
 
 
 
+const form = document.createElement("form")
 
 
+function projectForm(){  
+        form.innerHTML = `<label for="name">project name:</label>
+                        <input type="text" id="name" name="name">
+                    <button type="submit">create</button>`
+         nav.appendChild(form)
+
+}
 
 
 export function projectAdder(){ 
-    const form = document.createElement("form")
+    projectForm()
+
     form.addEventListener("submit" , (e)=>{  
         e.preventDefault()
         let value = form.querySelector("#name").value
         let newProject = new project(value)
         projects.push(newProject)
-        console.log(projects)
+
         
     })
-    form.innerHTML = `<label for="name">project name:</label>
-                        <input type="text" id="name" name="name">
-                    <button type="submit">create</button>`
-    nav.appendChild(form)
 
 
 
 }
 
+export function projectRemover(element){ 
+    for(let i = 0 ; i<projects.length ; i++){ 
+        if (projects[i].id == element.id){ 
+            projects.splice(i , 1)
+        }
+
+    }
+    console.log(projects)
+    
+}
 
 
 export {projects}
