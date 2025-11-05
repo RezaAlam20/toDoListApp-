c94104226dbda8d1427aa7236de6b4324c95e0ba
RezaAlam20 <rzaalam384@gmail.com>
2025-11-05 21:37:11 +0330

    made a new module for making the projects has classes for projects and tasks and methods for both

---
+++ src/project.js
@@ -0,0 +1,56 @@
+class project{ 
+    constructor(name){
+        this.name = name
+        this.id = crypto.randomUUID()
+        this.tasks = []
+
+    }
+
+    createTask(name , priority){
+        this.tasks.push(new Task(name , priority))
+        
+
+    }
+
+    deleteTask(id){ 
+        this.tasks.forEach(task => {
+            if (task.id == id){ 
+                this.tasks.splice(task , 1)
+
+            }
+            
+        }); 
+        
+        
+    }
+    editTask(){ 
+
+    }
+
+
+
+
+}
+
+class Task { 
+    constructor(name , priority , done=false ){  
+        this.name = name 
+        this.priority = priority
+        this.done = done 
+        this.id = crypto.randomUUID()
+    }
+
+    done() { 
+        // if clicked -> donecheck = true 
+    }
+
+
+
+}
+
+
+
+export {project} ;
+
+
+
