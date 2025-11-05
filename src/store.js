9a079b1af2e93e33743d562108bc25c29c34fe28
RezaAlam20 <rzaalam384@gmail.com>
2025-11-05 19:38:55 +0330

    changed the name of the project handler

---
+++ src/store.js
@@ -0,0 +1,48 @@
+
+const nav = document.querySelector("#nav")
+
+
+
+let projects = []
+export function projectHandler(){ 
+    const form = document.createElement("form")
+    form.addEventListener("submit" , (e)=>{  
+        e.preventDefault()
+        let value = form.querySelector("#name").value
+        let newProject = new project(value)
+        projects.push(newProject)
+        
+    })
+    form.innerHTML = `<label for="name">project name:</label>
+                        <input type="text" id="name" name="name">
+                    <button type="submit">create</button>`
+    nav.appendChild(form)
+
+}
+
+
+class project{ 
+    constructor(name){
+        this.name = name
+    }
+
+    createTask(){ 
+        
+    }
+
+    deleteTask(){  
+        
+    }
+
+
+
+}
+
+export {projects}
+
+
+
+
+
+
+
