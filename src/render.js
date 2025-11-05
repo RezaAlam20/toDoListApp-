22503b688c1fb9ad619c4cd0943572a91f26cc2c
RezaAlam20 <rzaalam384@gmail.com>
2025-11-05 22:58:44 +0330

    added removeBtn and editBtn and project id to the project div line 21-29 / 40-50

--- src/render.js
+++ src/render.js
@@ -1,15 +1,31 @@
+import { project } from "./project";
+import { projects } from "./store";
 
 
 const container = document.querySelector("#container")
 
 
-export function makeProjectsDivs(projects){
+
+
+
+
+
+
+
+export function renderProjects(projects){
     while(container.firstElementChild){ 
         container.removeChild(container.firstElementChild)
     }
     projects.forEach(project => {
         const div = document.createElement("div")
+        div.id = project.id
         container.appendChild(div)
+        const name = document.createElement("p")
+        name.textContent = project.name
+        div.appendChild(name)
+        removeProjectDiv(div)
+        editProject(div)
+
 
         
         
@@ -21,7 +37,17 @@ export function makeProjectsDivs(projects){
 
 }
 
-function makeTaskDivs(){ 
+function removeProjectDiv(div){  
+    const removeBtn = document.createElement("div")
+    removeBtn.classList.add("removeBtn")
+    div.appendChild(removeBtn)
+
+}
+
+function editProject(div){ 
+    const editProject = document.createElement("div")
+    editProject.classList.add("editProject")
+    div.appendChild(editProject)
 
 
 }
