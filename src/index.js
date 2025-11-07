a6f77a7d0f837ff5d386e52a60cee9ba4efeea93
RezaAlam20 <rzaalam384@gmail.com>
2025-11-07 19:52:34 +0330

    moved the form from the store.js to index.js and moved the activeproject to the store.js lines 12-46 / 61-63

--- src/index.js
+++ src/index.js
@@ -1,25 +1,48 @@
 import styleSheet from "./style.css"
 import { renderProjects } from "./render"
 import { projects } from "./store"
-import { projectAdder } from "./store"
 import { projectRemover } from "./store"
 import { renderTasks } from "./render"
 import { project } from "./project"
+import { setActiveProject } from "./store"
+import { getActiveProject } from "./store"
 
-let activeProject ;
+
+
+const form = document.createElement("form")
+
+function projectForm(){  
+
+
+        form.innerHTML = `<label for="name">project name:</label>
+                        <input type="text" id="name" name="name">
+                    <button type="submit">create</button>`
+         nav.appendChild(form)
+
+}
+
+
+
+function projectAdder(){ 
+    projectForm()
+
+    form.addEventListener("submit" , (e)=>{  
+        e.preventDefault()
+        let value = form.querySelector("#name").value
+        let newProject = new project(value)
+        projects.push(newProject)
+        renderProjects(projects)
+        const lastIndex = projects.length - 1
+        setActiveProject(projects[lastIndex])
+        renderTasks(getActiveProject())
+
+        
+    })
+
+
+
+}
 projectAdder()
-
-
-
-const form = document.querySelector("form")
-form.addEventListener("submit" , ()=>  { 
-    renderProjects(projects)
-    const lastIndex = projects.length - 1
-    activeProject = projects[lastIndex]
-    renderTasks(activeProject)
-
-
-})
 
 
 const container = document.querySelector("#container")
@@ -35,23 +58,13 @@ container.addEventListener("click" , (e)=> {
         const activeId = e.target.id 
         for(let i = 0 ; i<projects.length ; i++) { 
             if (projects[i].id == activeId){  
-                activeProject = projects[i]
-                console.log(activeProject)
-                renderTasks(activeProject)
+                const curproject = projects[i]
+                setActiveProject(curproject)
+                renderTasks(getActiveProject())
             }
         }
     }
 })
-
-
-
-
-export {activeProject}
-
-
-
-
-
 
 
 
