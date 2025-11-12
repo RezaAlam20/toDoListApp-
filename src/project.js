d40eae8468d0309af59d3150d3bcb317c4f791a1
RezaAlam20 <rzaalam384@gmail.com>
2025-11-12 21:32:39 +0330

    added dueStatus and functions for due date used datefns for unix date conversions

--- src/project.js
+++ src/project.js
@@ -1,3 +1,6 @@
+import { getUnixTime } from "date-fns";
+import { startOfDay } from "date-fns";
+
 class project{ 
     constructor(name){
         this.name = name
@@ -6,8 +9,10 @@ class project{
 
     }
 
-    createTask(name , priority){
-        this.tasks.push(new Task(name , priority))
+    createTask(name , priority , dueDate){
+        const task = new Task(name , priority ,dueDate)
+        this.tasks.push(task)
+        return task
         
 
     }
@@ -33,15 +38,34 @@ class project{
 }
 
 class Task { 
-    constructor(name , priority , done=false ){  
+    constructor(name , priority , dueDate , done=false , dueStatus ){  
         this.name = name 
         this.priority = priority
         this.done = done 
         this.id = crypto.randomUUID()
+        this.dueDate = dueDate
+        this.dueStatus = dueStatus 
     }
     taskDone() { 
         this.done =  !this.done
     }
+    checkDue() {
+    const now = getUnixTime(startOfDay(new Date()))
+    const unix = getUnixTime(startOfDay(this.dueDate))
+    if (unix == now ){ 
+        this.dueStatus = "due-today"
+    }
+    else if (unix > now){ 
+        this.dueStatus = "upcoming"
+    }
+    else if (unix < now){ 
+        this.dueStatus = "over-due"
+    }
+
+
+}
+
+
 
 
 
